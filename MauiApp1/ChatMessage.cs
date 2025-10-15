b11f1e85bc3c32699e0024918a8feccc94fc5780
4189594 <4189594@TE-GDA-D-D21E06>
2025-10-15 13:06:44 +0200

    zadanie

---
+++ MauiApp1/ChatMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    internal class ChatMessage
+    {
+        public string Autor { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,14 +1,80 @@
-﻿namespace MauiApp1
+﻿using System.Data;
+using System.Net.Http.Json;
+using System.Web;
+
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
-
-
+        DateTime lastChatRefresh;
+        IDispatcherTimer timer;
         public MainPage()
         {
             InitializeComponent();
+            lastChatRefresh = DateTime.UnixEpoch;
+
+            timer = Application.Current.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += (s, e) =>
+            {
+                GetHistory();
+            };
+            timer.Start();
         }
 
+        private void Send(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string message = ChatEntry.Text;
 
+            ChatEntry.Text = string.Empty;
+            //Label NewMessageLabel = new Label();
+            //NewMessageLabel.Text = username + ": " + message;
+            //ChatHistory.Children.Add(NewMessageLabel);
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7159");
+                ChatMessage chatMessage = new ChatMessage
+                {
+                    Autor = username,
+                    Content = message,
+                    Timestamp = DateTime.Now
+                };
+                HttpResponseMessage response = client.PostAsJsonAsync("chat", chatMessage).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    //Message sent successfully
+                }
+                else
+                {
+                    DisplayAlert("Error", "Failed to send message", "OK");
+                }
+            }
+        }
+        private void GetHistory()
+        {
+            string timestamp = lastChatRefresh.ToString("o");
+            timestamp = HttpUtility.UrlEncode(timestamp);
+            //blok using żeby automatycznie zamknąć klienta po zakończeniu
+            using (HttpClient client = new HttpClient())
+            {
+                //ustawienie adresu bazowego
+                client.BaseAddress = new Uri("https://localhost:7159/");
+                //wykonanie żądania GET do endpointu /chat
+                HttpResponseMessage response = client.GetAsync("chat?timestamp=" + timestamp).Result;
+                //parsuj odpowiedź jako listę obiektów typu ChatMessage
+                List<ChatMessage> messages = response.Content.ReadFromJsonAsync<List<ChatMessage>>().Result
+                                                                ?? new List<ChatMessage>();
+                //dopisujemy je do historii czatu
+                foreach (ChatMessage message in messages)
+                {
+                    Label messageLabel = new Label();
+                    messageLabel.Text = message.Timestamp + " " + message.Autor + ": " + message.Content;
+                    ChatHistory.Children.Add(messageLabel);
+                }
+            }
+            ChatScrollView.ScrollToAsync(ChatHistory, ScrollToPosition.End, true);
+            lastChatRefresh = DateTime.Now;
+        }
     }
 }
